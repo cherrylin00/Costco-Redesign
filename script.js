5c460c1cf5e6ae7d06eb43a72ed0c69c2015ebe1
cherrylin00 <cherrylin.design@gmail.com>
2024-06-18 00:57:46 +0800

    goodnight-2457

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
-console.log("Script Loaded");
-// Header的SearchBar
-document.getElementById('search-icon').addEventListener('click', function(event) {
-    event.preventDefault();
-    document.getElementById('search-bar-container').style.display = 'flex';
+
+// Header的SearchBar點擊
+document.addEventListener('DOMContentLoaded', function() {
+    var searchIcon = document.getElementById('search-icon');
+    var searchBarContainer = document.getElementById('search-bar-container');
+    var closeBtn = document.getElementById('close-btn');
+
+    searchIcon.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevent the click event from bubbling up
+        searchBarContainer.style.display = 'flex';
+    });
+
+    closeBtn.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevent the click event from bubbling up
+        searchBarContainer.style.display = 'none';
+    });
+
+    document.addEventListener('click', function() {
+        searchBarContainer.style.display = 'none';
+    });
+
+    searchBarContainer.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevent clicks inside the search bar from closing it
+    });
 });
 
-document.getElementById('close-btn').addEventListener('click', function() {
-    document.getElementById('search-bar-container').style.display = 'none';
-});
 
-// Apply Btn點擊後 --> 會員等級頁面
+
+// Header-手機版的漢堡點擊，下拉選單會出來
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuIcon = document.querySelector('.bi-list-sm');
+    var mainNavSm = document.querySelector('.main-nav-sm');
+    var items = document.querySelectorAll('.sm-item');
+
+    menuIcon.addEventListener('click', function(event) {
+        event.stopPropagation(); // Prevent the click event from bubbling up
+        if (mainNavSm.style.display === 'none' || mainNavSm.style.display === '') {
+            mainNavSm.style.display = 'flex'; // 使用 flex 以確保布局正確
+        } else {
+            mainNavSm.style.display = 'none';
+        }
+    });
+
+    items.forEach(function(item) {
+        item.addEventListener('click', function(event) {
+            event.stopPropagation(); // Prevent the click event from bubbling up
+            var dropdown = this.nextElementSibling;
+            if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+                dropdown.style.display = 'block';
+            } else {
+                dropdown.style.display = 'none';
+            }
+        });
+    });
+
+    document.addEventListener('click', function(event) {
+        var isClickInsideMenu = mainNavSm.contains(event.target) || menuIcon.contains(event.target);
+        if (!isClickInsideMenu) {
+            mainNavSm.style.display = 'none';
+        }
+    });
+});
