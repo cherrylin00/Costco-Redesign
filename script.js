9027d8a4ad6c9221eb2670afc32e19bfca6ca99e
cherrylin00 <cherrylin.design@gmail.com>
2024-06-17 15:49:50 +0800

    global-0617

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("Script Loaded");
+// Header的SearchBar
 document.getElementById('search-icon').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('search-bar-container').style.display = 'flex';
@@ -7,3 +9,4 @@ document.getElementById('close-btn').addEventListener('click', function() {
     document.getElementById('search-bar-container').style.display = 'none';
 });
 
+// Apply Btn點擊後 --> 會員等級頁面
