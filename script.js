d8dd119763c301352b9fb9b3908d873c46fca95a
cherrylin00 <cherrylin.design@gmail.com>
2024-06-19 07:54:35 +0800

    miss-#

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Header的SearchBar點擊
 document.addEventListener('DOMContentLoaded', function() {
     var searchIcon = document.getElementById('search-icon');
@@ -22,12 +21,23 @@ document.addEventListener('DOMContentLoaded', function() {
     searchBarContainer.addEventListener('click', function(event) {
         event.stopPropagation(); // Prevent clicks inside the search bar from closing it
     });
+
+    // 新增防止 URL 中出現不必要的 # 符號的部分
+    window.addEventListener('hashchange', function() {
+        if (window.location.hash === '#') {
+            history.replaceState('', document.title, window.location.pathname);
+        }
+    }, false);
+
+    // 阻止空錨點的預設行為
+    document.querySelectorAll('a[href="#"]').forEach(function(anchor) {
+        anchor.addEventListener('click', function(event) {
+            event.preventDefault();
+        });
+    });
 });
 
-
-
 // Header-手機版的漢堡點擊，下拉選單會出來
-
 document.addEventListener('DOMContentLoaded', function() {
     var menuIcon = document.querySelector('.bi-list-sm');
     var mainNavSm = document.querySelector('.main-nav-sm');
